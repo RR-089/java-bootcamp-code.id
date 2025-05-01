b8da4ba2d6fd40045b31a337c4ddec9043a341ec
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-05-01 16:42:16 +0700

    add: simple fragment example (spring mvc)

--- spring-mvc/eshopeer/src/main/java/com/codeid/eshopeer/EshopeerApplication.java
+++ spring-mvc/eshopeer/src/main/java/com/codeid/eshopeer/EshopeerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopeerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EshopeerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EshopeerApplication.class, args);
+    }
 
 }
