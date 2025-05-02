e13d9fa9e189ba2b915541d4152e35713f9bfba4
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-05-02 17:25:17 +0700

    add: day 2 challenge, CRUD Category and layout (spring mvc)

--- spring-mvc/eshopeer/src/main/java/com/codeid/eshopeer/controller/CategoryController.java
+++ spring-mvc/eshopeer/src/main/java/com/codeid/eshopeer/controller/CategoryController.java
@@ -8,9 +8,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categories")
@@ -23,21 +25,22 @@ public class CategoryController {
 
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-        //TODO: consider use dto
         binder.setDisallowedFields("picture");
     }
 
     @GetMapping
     public String findAllCategories(Model model) {
-        List<Category> categories = categoryService.findAllCategories();
-        model.addAttribute("categories", categories);
+        commonAttributes(model);
         return "categories/index";
     }
 
     @GetMapping("/create")
     public String showCreateForm(Model model) {
+        commonAttributes(model);
+
         model.addAttribute("category", new Category());
-        return "categories/create";
+        model.addAttribute("action", "Create Category");
+        return "categories/upsert";
     }
 
     @PostMapping
@@ -50,17 +53,46 @@ public class CategoryController {
             result.getAllErrors().forEach(error ->
                     System.out.println("Validation error: " + error.getDefaultMessage())
             );
-            return "categories/create";
+            return "categories/upsert";
         }
 
         try {
             categoryService.saveCategory(category, picture);
         } catch (IOException e) {
             result.rejectValue("picture", "error.category", "Failed to upload picture");
-            return "categories/create";
+            return "categories/upsert";
+
         }
 
         return "redirect:/categories";
+    }
+
+
+    @GetMapping("/update/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        Optional<Category> category = categoryService.findCategoryById(id);
+
+        commonAttributes(model);
+        model.addAttribute("category", category.get());
+        model.addAttribute("action", "Update Category");
+        return "categories/upsert";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable("id") Long id, RedirectAttributes redirectAttributes,
+                                 Model model) {
+        commonAttributes(model);
+        categoryService.deleteCategory(id);
+        redirectAttributes.addFlashAttribute("message", "Category ID:  " + id +
+                " has been delete!");
+
+        return "redirect:/categories";
+    }
+
+
+    private void commonAttributes(Model model) {
+        List<Category> categories = categoryService.findAllCategories();
+        model.addAttribute("categories", categories);
     }
 
 
