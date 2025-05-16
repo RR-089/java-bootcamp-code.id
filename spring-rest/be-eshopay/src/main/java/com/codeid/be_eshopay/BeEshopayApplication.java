3e80ef5f2e79c33428d03091adbeb610b329f6df
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-05-16 08:15:19 +0700

    chore: enable transaction management

--- spring-rest/be-eshopay/src/main/java/com/codeid/be_eshopay/BeEshopayApplication.java
+++ spring-rest/be-eshopay/src/main/java/com/codeid/be_eshopay/BeEshopayApplication.java
@@ -2,12 +2,14 @@ package com.codeid.be_eshopay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BeEshopayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeEshopayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BeEshopayApplication.class, args);
+    }
 
 }
